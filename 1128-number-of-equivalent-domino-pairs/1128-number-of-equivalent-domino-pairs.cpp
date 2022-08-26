564ff735c313c5cb98d5aff6fef07d21f9c87936
Jasper Lin <73963814+JasperLin0118@users.noreply.github.com>
2022-08-26 17:38:32 +0800

    Time: 92 ms (41.77%), Space: 22 MB (68.88%) - LeetHub

---
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) 
+    {
+        int total = 0;
+        map<pair<int, int>, int> dict;
+        for(int i=0;i<dominoes.size();i++)
+        {
+            pair<int, int> p = make_pair(dominoes[i][0], dominoes[i][1]);
+            pair<int, int> rp = make_pair(dominoes[i][1], dominoes[i][0]);
+            if(dict.find(p) != dict.end())
+                dict[p]++;
+            else if(dict.find(rp) != dict.end())
+                dict[rp]++;
+            else
+                dict[p]++;
+        }
+        for(auto data : dict)
+        {
+            if(data.second > 1)
+            {
+                for(int i=1;i<data.second;i++)
+                    total += data.second - i;
+            }
+        }
+        return total;
+    }
+};

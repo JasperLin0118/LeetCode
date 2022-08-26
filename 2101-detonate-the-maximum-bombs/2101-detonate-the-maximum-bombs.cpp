1c41fbb3e28f4efcbe7686eb681eda5962e841e5
Jasper Lin <73963814+JasperLin0118@users.noreply.github.com>
2022-08-26 19:02:21 +0800

    Time: 379 ms (31.27%), Space: 14.9 MB (74.07%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximumDetonation(vector<vector<int>>& bombs) 
     {
-        vector<vector<int>> adj_mat(bombs.size(), vector<int>(bombs.size(), 0));
+        vector<vector<int>> adj_mat(bombs.size(), vector<int>());
         for(int i=0;i<bombs.size();i++)
         {
             for(int j=0;j<bombs.size();j++)
@@ -11,7 +11,7 @@ public:
                     continue;
                 double dist = sqrt(pow(bombs[i][1] - bombs[j][1], 2) + pow(bombs[i][0] - bombs[j][0], 2));
                 if(dist <= bombs[i][2])
-                    adj_mat[i][j] = 1;
+                    adj_mat[i].push_back(j);
             }
         }
         int ans = 0;
@@ -27,10 +27,10 @@ public:
     void dfs(vector<vector<int>> &graph, int v, vector<bool> &visit, int &cnt)
     {
         visit[v] = true;
-        for(int i=0;i<graph.size();i++)
+        for(int i=0;i<graph[v].size();i++)
         {
-            if(graph[v][i] == 1 && !visit[i]) 
-                dfs(graph, i, visit, ++cnt);
+            if(!visit[graph[v][i]])
+                dfs(graph, graph[v][i], visit, ++cnt);
         }
     }
 };

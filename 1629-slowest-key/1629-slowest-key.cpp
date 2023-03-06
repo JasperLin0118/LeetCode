7fcd1be950f474fd7079404efe2ea891ede1667f
Jasper Lin <73963814+JasperLin0118@users.noreply.github.com>
2023-03-06 14:51:45 +0800

    Time: 11 ms (52.97%), Space: 10.8 MB (62.53%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.cpp
+++ 1629-slowest-key/1629-slowest-key.cpp
@@ -6,7 +6,6 @@ public:
         char c = keysPressed[0];
         for(int i=1;i<releaseTimes.size();i++)
         {
-            cout << releaseTimes[i] - releaseTimes[i-1] << " ";
             if(releaseTimes[i] - releaseTimes[i-1] > mx)
             {
                 mx = releaseTimes[i] - releaseTimes[i-1];
